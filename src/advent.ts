3a5fc8c5da5e0dba8197cb8a9bcc6030658199b6
Sean Metzgar <sean.metzgar@gmail.com>
2022-12-03 00:41:43 -0500

    🎅Adds day 2

--- src/advent.ts
+++ src/advent.ts
@@ -10,3 +10,9 @@ const day01 = Puzzles.day01(Data.day01);
 console.log('Day 1:', day01);
 console.groupEnd();
 lb();
+
+console.group('Day 2');
+const day02 = Puzzles.day02(Data.day02);
+console.log('Day 2:', day02);
+console.groupEnd();
+lb();
