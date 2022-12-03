b8d4fa0f4b4c0377059932f3aeb0f34a37dc27f5
Sean Metzgar <sean.metzgar@gmail.com>
2022-12-03 04:06:45 -0500

    🧹Clean up advent.ts

--- src/advent.ts
+++ src/advent.ts
@@ -7,20 +7,15 @@ const lb = () => {
 
 // Get number of keys in object Puzzle
 
-
-
-
-
 // Loop through each day
 for (let day = 1; day <= Object.keys(Puzzles).length; day++) {
   console.group('Day ' + day);
-  let dayString = 'day' + (('00' + day).slice(-2));
+  let dayString = 'day' + ('00' + day).slice(-2);
   if (Data.hasOwnProperty(dayString) && Puzzles.hasOwnProperty(dayString)) {
     let data = Data[dayString];
     let puzzle = Puzzles[dayString](data);
     console.log('Results: ', puzzle);
-  }
-  else {
+  } else {
     console.warn('No data or puzzle for day ' + day);
   }
   console.groupEnd();
