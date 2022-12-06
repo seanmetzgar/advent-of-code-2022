c73bc4bdbdec47e94958dcb89f548e7d00252f00
Sean Metzgar <sean.metzgar@gmail.com>
2022-12-06 13:24:35 -0500

    ☃️Day 6 - Adds both puzzle solutions for day 6 - Adds a template file in the src folder for new days

--- src/puzzles/index.ts
+++ src/puzzles/index.ts
@@ -5,11 +5,13 @@ import Day02 from './day02';
 import Day03 from './day03';
 import Day04 from './day04';
 import Day05 from './day05';
+import Day06 from './day06';
 
 export default {
   day01: Day01,
   day02: Day02,
   day03: Day03,
   day04: Day04,
-  day05: Day05
+  day05: Day05,
+  day06: Day06
 } as Puzzles
