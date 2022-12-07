7b9050e694e3d20d722da8a5122b01dcec988866
Sean Metzgar <sean.metzgar@gmail.com>
2022-12-07 15:53:29 -0500

    🎅🏻Adds Day 7 Solutions

--- src/puzzles/index.ts
+++ src/puzzles/index.ts
@@ -6,6 +6,7 @@ import Day03 from './day03';
 import Day04 from './day04';
 import Day05 from './day05';
 import Day06 from './day06';
+import Day07 from './day07';
 
 export default {
   day01: Day01,
@@ -13,5 +14,6 @@ export default {
   day03: Day03,
   day04: Day04,
   day05: Day05,
-  day06: Day06
-} as Puzzles
+  day06: Day06,
+  day07: Day07,
+} as Puzzles;
