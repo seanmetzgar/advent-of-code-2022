90b6b6b0688bf9519afcfb3b2e7d01ceb848bafa
Sean Metzgar <sean.metzgar@gmail.com>
2022-12-20 23:28:01 -0500

    Adds puzzle 2 for day 13

--- src/puzzles/day13.ts
+++ src/puzzles/day13.ts
@@ -1,8 +1,8 @@
 import { Solution } from '../types';
 
-type CompareItem = number | undefined | CompareItem[];
+type Packet = number | undefined | Packet[];
 
-const compare = (a: CompareItem, b: CompareItem): boolean | undefined => {
+const compare = (a: Packet, b: Packet): boolean | undefined => {
 	if (typeof a === 'number' && typeof b === 'number') {
 		return a > b ? false : a < b ? true : undefined;
 	} else if (Array.isArray(a) !== Array.isArray(b)) {
@@ -23,25 +23,57 @@ const compare = (a: CompareItem, b: CompareItem): boolean | undefined => {
 	return undefined;
 }
 
-const puzzle1 = (group1: CompareItem[], group2: CompareItem[]): number => {
+const same = (a: Packet, b: Packet): boolean => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a === b;
+  }
+  if (Array.isArray(a) !== Array.isArray(b)) {
+    return false;
+  }
+  if (Array.isArray(a) && Array.isArray(b)) {
+    for (let i = 0, end = Math.max(a.length, b.length); i < end; i++) {
+      if (a[i] === undefined || b[i] === undefined) return false;
+      if (!same(a[i], b[i])) return false;
+    }
+  }
+  return true;
+}
+
+const puzzle1 = (packetA: Packet[], packetB: Packet[]): number => {
   let count = 0;
-  for (let i: number = 0; i < Math.max(group1.length, group2.length); i++) {
-    count += compare(group1[i], group2[i]) ? i + 1 : 0;
+  for (let i: number = 0; i < Math.max(packetA.length, packetB.length); i++) {
+    count += compare(packetA[i], packetB[i]) ? i + 1 : 0;
 
   }
 
   return count;
 }
 
+const puzzle2 = (packetsA: Packet[], packetsB: Packet[], decoderPackets: Packet[]): number => {
+  const stream = [
+    ...packetsA,
+    ...packetsB,
+		...decoderPackets
+	].sort((a, b) => {
+		const result = compare(a, b);
+		return result === undefined ? 0 : result ? -1 : 1;
+	});
+
+  return decoderPackets.reduce((accumulator: number, packet) => 
+    accumulator * (1 + stream.findIndex(p => 
+      JSON.stringify(p) === JSON.stringify(packet)
+    )), 1);
+}
+
 export default (dataSet: string): Solution => {
-  const group1: CompareItem[] = [];
-  const group2: CompareItem[] = [];
+  const packetsA: Packet[] = [];
+  const packetsB: Packet[] = [];
 
   dataSet.split('\n\n').forEach((group: string) => {
     const parts = group.split('\n');
-    group1.push(JSON.parse(parts[0]));
-    group2.push(JSON.parse(parts[1]));
+    packetsA.push(JSON.parse(parts[0]));
+    packetsB.push(JSON.parse(parts[1]));
   });
-
-  return { puzzle1: puzzle1(group1, group2), puzzle2: 0 };
+  
+  return { puzzle1: puzzle1(packetsA, packetsB), puzzle2: puzzle2(packetsA, packetsB, [[[2]], [[6]]]) };
 };
